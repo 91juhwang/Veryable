65dacb9a8aa16e0d5039bf9e3927dc82de924a27
James Hwang <91juhwang@gmail.com>
2026-01-16 19:55:33 -0600

    Use UTC for datetime formatting

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -1,5 +1,7 @@
+const DISPLAY_TIME_ZONE = "America/Chicago";
+
 const DATE_TIME_FORMAT = new Intl.DateTimeFormat("en-US", {
-  timeZone: "UTC",
+  timeZone: DISPLAY_TIME_ZONE,
   year: "numeric",
   month: "2-digit",
   day: "2-digit",
@@ -8,7 +10,7 @@ const DATE_TIME_FORMAT = new Intl.DateTimeFormat("en-US", {
 });
 
 const TIME_FORMAT = new Intl.DateTimeFormat("en-US", {
-  timeZone: "UTC",
+  timeZone: DISPLAY_TIME_ZONE,
   hour: "2-digit",
   minute: "2-digit",
 });
